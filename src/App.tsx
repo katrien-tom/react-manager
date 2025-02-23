902dcce476603fcfd5a5e64896968134dbac45a1
banyejiu <banyejiu@gmail.com>
2025-02-23 15:52:51 +0800

    初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="header">
+          <h1>React Manager</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
