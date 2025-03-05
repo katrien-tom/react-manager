6823b387b3d5a53939b2e74311d8365838da664f
gaofei <1050456851@qq.com>
2025-03-05 16:02:16 +0800

    10-6头像上传完成

--- src/views/modules/system/user/CreateUser.tsx
+++ src/views/modules/system/user/CreateUser.tsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { Form, Input, Upload, Modal, Select} from "antd";
-import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
-import { RcFile, UploadChangeParam } from "antd/es/upload";
+import { Form, Input, Upload, Modal, Select, Image } from 'antd';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { RcFile, UploadChangeParam } from 'antd/es/upload';
 
-import storage from "@/utils/storage";
-import { message } from "@/components/AntdGlobal";
+import storage from '@/utils/storage';
+import { message } from '@/components/AntdGlobal';
 
 const CreateUser = () => {
   const [open, setOpen] = useState(true);
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
+  const [imgUrl, setImgUrl] = useState('');
   const handleSubmit = async () => {
     const validate = await form.validateFields();
     console.log('submit', validate);
@@ -34,76 +35,87 @@ const CreateUser = () => {
   };
   // 上传图片后的钩子
   const handleChange = (info: UploadChangeParam) => {
-    if(info.file.status === 'uploading'){
+    if (info.file.status === 'uploading') {
       setLoading(true);
       return;
     }
     if (info.file.status === 'done') {
-      message.success('上传成功');
       setLoading(false);
+      // 服务端返回的数据
+      const { code, data, msg } = info.file.response;
+      if (code === 0) {
+        message.success('上传成功');
+        setImgUrl(data.url);
+      } else {
+        message.error(msg);
+      }
     } else if (info.file.status === 'error') {
-      message.error('上传失败');
+      message.error('服务异常，请稍后再试');
       setLoading(false);
     }
   };
   return (
-  <div className="create-user">
-    <Modal
-    title="创建用户"
-    okText="提交"
-    cancelText="取消"
-    width={800}
-    open={open}
-    onOk={handleSubmit}
-    onCancel={handleCancel}
-    >
-      <Form form={form} layout="vertical" labelCol={{ span: 4 }} labelAlign="right">
-        <Form.Item label="用户名称" name="userName" rules={[{ required: true, message: '请输入用户名称' }]}>
-          <Input placeholder="请输入用户名称" />
-        </Form.Item>
-        <Form.Item label="用户邮箱" name="userEmail" rules={[{ required: true, message: '请输入用户邮箱' }]}>
-          <Input placeholder="请输入用户邮箱" />
-        </Form.Item>
-        <Form.Item label="手机号" name="mobile">
-          <Input type="number" placeholder="请输入手机号" />
-        </Form.Item>
-        <Form.Item label="部门" name="deptId" rules={[{ required: true, message: '请输入部门' }]}>
-          <Input placeholder="请输入部门" />
-        </Form.Item>
-        <Form.Item label="岗位" name="job">
-          <Input placeholder="请选择岗位" />
-        </Form.Item>
-        <Form.Item label="状态" name="state">
-          <Select placeholder="请选择状态" style={{ width: 120 }}>
-            <Select.Option value={0}>所有</Select.Option>
-            <Select.Option value={1}>在职</Select.Option>
-            <Select.Option value={2}>离职</Select.Option>
-            <Select.Option value={3}>试用期</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item label="角色" name="roleList">
-         <Input placeholder="请输入角色" />
-        </Form.Item>
-        <Form.Item label="用户头像">
-          <Upload
-          listType="picture-circle"
-          showUploadList={false}
-          headers={{
-            Authorization: `Bearer ${storage.get('token')}`
-          }}
-          action='/api/users/uploads'
-          beforeUpload={beforeUpload}
-          onChange={handleChange}
-          >
-            <div>
-              {loading ? <LoadingOutlined /> : <PlusOutlined />}
-            <div style={{ marginTop: 5 }}>上传图像</div>
-            </div>
-          </Upload>
-        </Form.Item>
-      </Form>
-    </Modal>
-  </div>
+    <div className='create-user'>
+      <Modal
+        title='创建用户'
+        okText='提交'
+        cancelText='取消'
+        width={800}
+        open={open}
+        onOk={handleSubmit}
+        onCancel={handleCancel}
+      >
+        <Form form={form} layout='vertical' labelCol={{ span: 4 }} labelAlign='right'>
+          <Form.Item label='用户名称' name='userName' rules={[{ required: true, message: '请输入用户名称' }]}>
+            <Input placeholder='请输入用户名称' />
+          </Form.Item>
+          <Form.Item label='用户邮箱' name='userEmail' rules={[{ required: true, message: '请输入用户邮箱' }]}>
+            <Input placeholder='请输入用户邮箱' />
+          </Form.Item>
+          <Form.Item label='手机号' name='mobile'>
+            <Input type='number' placeholder='请输入手机号' />
+          </Form.Item>
+          <Form.Item label='部门' name='deptId' rules={[{ required: true, message: '请输入部门' }]}>
+            <Input placeholder='请输入部门' />
+          </Form.Item>
+          <Form.Item label='岗位' name='job'>
+            <Input placeholder='请选择岗位' />
+          </Form.Item>
+          <Form.Item label='状态' name='state'>
+            <Select placeholder='请选择状态' style={{ width: 120 }}>
+              <Select.Option value={0}>所有</Select.Option>
+              <Select.Option value={1}>在职</Select.Option>
+              <Select.Option value={2}>离职</Select.Option>
+              <Select.Option value={3}>试用期</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label='角色' name='roleList'>
+            <Input placeholder='请输入角色' />
+          </Form.Item>
+          <Form.Item label='用户头像'>
+            <Upload
+              listType='picture-circle'
+              showUploadList={false}
+              headers={{
+                Authorization: `Bearer ${storage.get('token')}`,
+              }}
+              action='/api/users/uploads'
+              beforeUpload={beforeUpload}
+              onChange={handleChange}
+            >
+              {imgUrl ? (
+                <Image src={imgUrl} alt='avatar' width={100} style={{ borderRadius: '50%' }} />
+              ) : (
+                <div>
+                  {loading ? <LoadingOutlined /> : <PlusOutlined />}
+                  <div style={{ marginTop: 5 }}>上传图像</div>
+                </div>
+              )}
+            </Upload>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
   );
 };
 
